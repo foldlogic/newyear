fcf5c8bd9095542a2d0e4ed6ef7874264d77d3d6
Zbigniew Siciarz <zbigniew@siciarz.net>
2014-12-31 23:25:09 +0100

    It works!

---
+++ Main.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad.Trans (liftIO)
+import Data.Time
+
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Text.Blaze.Html5.Attributes (class_, href, rel)
+import Web.Scotty
+
+getCurrentYear :: IO Integer
+getCurrentYear = do
+    (year, _, _) <- fmap (toGregorian . localDay . zonedTimeToLocalTime) getZonedTime
+    return year
+
+main :: IO ()
+main = scotty 3000 $ do
+    get "/" $ do
+        year <- liftIO getCurrentYear
+        let answer = if year == 2015 then "Yes! Open the champagne!" else "No, not yet."
+        html $ renderHtml $
+            H.html $ do
+                H.head $ do
+                    H.title "Is it 2015 yet?"
+                    H.link H.! href "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" H.! rel "stylesheet"
+                H.body $ do
+                    H.div H.! class_ "container" $ do
+                        H.div H.! class_ "jumbotron" $ do
+                            H.h1 "Is it 2015 yet?"
+                            H.p answer
+
